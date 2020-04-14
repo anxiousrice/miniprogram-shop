4447cecb21d4064a81a882e45a197c3b6a70ed0a
lideren <2379196845@qq.com>
2020-04-14 22:47:48 +0800

    thirdd

--- request/index.js
+++ request/index.js
@@ -1,4 +1,12 @@
+//同时发送异步代码的次数
+let ajaxTimes = 0;
 export const request = (params) => {
+    ajaxTimes++ //发送一次请求就加一次
+    // 在请求 数据回来之前 显示一个加载中
+    wx.showLoading({
+        title: '加载中',
+    })
+
     //定义公共url
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
     return new Promise((resolve, reject) => {
@@ -10,6 +18,13 @@ export const request = (params) => {
             },
             fail: (err) => {
                 reject(err);
+            },
+            complete: () => {
+                ajaxTimes-- //请求完成一次 ，就减一次
+                if (ajaxTimes === 0) {
+                    //当所有的请求都完成时，就关闭 加载中 弹框
+                    wx.hideLoading()
+                }
             }
         });
     })
